cec2d8bd4e611557f5a41019d3ef6eeb88d49ea9
fauortiz <fauortiz@gmail.com>
2023-01-21 09:52:42 +0800

    test commit

--- pages/index.js
+++ pages/index.js
@@ -1,23 +1,38 @@
 import Head from 'next/head'
 import Header from '@components/Header'
-import Footer from '@components/Footer'
+// import Footer from '@components/Footer'
+import Hero from '@components/Hero'
+import Navbar from '@components/Navbar'
+import { useState, useEffect } from 'react'
+
+
 
 export default function Home() {
-  return (
-    <div className="container">
-      <Head>
-        <title>Next.js Starter!</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
 
-      <main>
-        <Header title="Welcome to my app!" />
-        <p className="description">
-          Get started by editing <code>pages/index.js</code>
-        </p>
-      </main>
+    const heroHeight = 400
+    // const [constants, setConstants] = useState(1234)
+    // useEffect(() => {
+    //     setConstants(document.querySelector('.hero').clientHeight)
+    // }, [])
+    // console.log(constants)
 
-      <Footer />
-    </div>
-  )
+    return (
+        <div className="all">
+            <Head>
+                <title>Francis Ortiz</title>
+                <link rel="icon" href="/favicon.ico" />
+            </Head>
+
+            <Hero />
+            <Navbar heroHeight={heroHeight} />
+            <main>
+                <Header title="header!" />
+                <p className="description">
+                    Get started by editing <code>pages/index.js</code>
+                </p>
+            </main>
+
+            {/* <Footer /> */}
+        </div>
+    )
 }
