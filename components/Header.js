622efc4671c71e11227112bab0e744ad2b7782ac
fauortiz <fauortiz@gmail.com>
2023-01-24 19:10:23 +0800

    test commit

--- components/Header.js
+++ components/Header.js
@@ -8,28 +8,28 @@ export default function Header({ isDesktop }) {
     const [showToggler, setShowToggler] = useState(true);
     const [lastScrollY, setLastScrollY] = useState(0);
     
-    // // show/hide navbar on scroll up/down
-    // useEffect(() => {
-    //     function toggleShowNavbar() {
+    // show/hide navbar on scroll up/down
+    useEffect(() => {
+        function toggleShowNavbar() {
 
-    //         // determine if user scrolled up
-    //         // console.log(`${window.scrollY} < ${lastScrollY}`)
-    //         if (window.scrollY < lastScrollY) {
-    //             if (isDesktop) {
-    //                 setShowNav(true)
-    //             }
-    //             setShowToggler(true)
-    //         } else {
-    //             setShowNav(false)
-    //             setShowToggler(false)
-    //         }
-    //         // remember last scroll position
-    //         setLastScrollY(window.scrollY)
-    //     }
+            // determine if user scrolled up
+            // console.log(`${window.scrollY} < ${lastScrollY}`)
+            if (window.scrollY < lastScrollY) {
+                // if (isDesktop) {
+                //     setShowNav(true)
+                // }
+                setShowToggler(true)
+            } else {
+                // setShowNav(false)
+                setShowToggler(false)
+            }
+            // remember last scroll position
+            setLastScrollY(window.scrollY)
+        }
 
-    //     window.addEventListener('scroll', toggleShowNavbar)
-    //     return () => window.removeEventListener('scroll', toggleShowNavbar)
-    // }, [lastScrollY])
+        window.addEventListener('scroll', toggleShowNavbar)
+        return () => window.removeEventListener('scroll', toggleShowNavbar)
+    }, [lastScrollY])
 
     // smooth scrolling anchor navigation
     useEffect(() => {
@@ -46,18 +46,28 @@ export default function Header({ isDesktop }) {
     }, [])
 
 
+    function toggleNav() {
+        setShowNav(prev => !prev)
+        setShowToggler(true)
+    }
+
     console.log('Navbar')
     return (
         <header>
-            <button className={`${styles.toggler} ${showToggler ? '' : styles.hide} ${showNav ? styles.navShow : ''}`} onClick={() => setShowNav(prev => !prev)}>
+            <button className={`${styles.toggler} ${showToggler ? '' : styles.hide} ${showNav ? styles.navShow : ''}`} onClick={toggleNav}>
                 <div className={styles.transparent}></div>
                 <div className={styles.opaque}></div>
-                <div className={styles.icon}><div>{`${showNav ? 'x' : '='}`}</div></div>
+                <div className={styles.logo}>
+                    <div>F</div>
+                    </div>
+                <div className={styles.burger}>
+                    <div>{`${showNav ? 'x' : '='}`}</div>
+                    </div>
             </button>
 
             <nav className={`${styles.nav} ${showNav ? styles.show : styles.hide}`}>
 
-                <button className={styles.closer} onClick={() => setShowNav(prev => !prev)}>
+                <button className={styles.closer} onClick={toggleNav}>
                     <div>F</div>    
                     </button>
                 <a href="#Home" className={`${styles.button} ${styles.home}`}>
@@ -72,9 +82,9 @@ export default function Header({ isDesktop }) {
                 <a href="#Contact" className={`${styles.button} ${styles.contact}`}>
                     <div className={styles.link}>Contact</div>
                     </a>
-                <a href="#Contact" className={`${styles.button} ${styles.resume}`}>
-                    <div className={styles.link}>View my Resume</div>
-                    </a>
+                <div className={`${styles.resume}`}>
+                    <a href="#Contact" className={styles.link}>View my Resume</a>
+                    </div>
             </nav>
         </header>
     )
