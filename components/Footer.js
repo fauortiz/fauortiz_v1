72248438608a7b6a23cb0dfea9d104a46dfe212e
fauortiz <fauortiz@gmail.com>
2023-02-15 14:19:51 +0800

    clean up default files

--- components/Footer.js
+++
@@ -1,9 +0,0 @@
-import styles from './Footer.module.css'
-
-export default function Footer() {
-    return (
-        <footer className={`${styles.footer} ${styles.potato}`}>
-            Made with <img src="/netliheart.svg" alt="Netlify Logo" className={styles.logo} /> for you
-        </footer>
-    )
-}
